9d007c4f3e9e41d3336338c60bf08f4cd879c41d
yukanke-ec <ykanke@ecbeing.co.jp>
2019-12-13 13:43:25 +0900

    PWA

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,30 @@
+var cacheName = 'SaaB-Tech';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/agency.min.css',
+  '/js/agency.min.js',
+  '/vendor/bootstrap/css/bootstrap.min.css',
+  '/vendor/fontawesome-free/css/all.min.css',
+  '/vendor/jquery/jquery.min.js',
+  '/vendor/bootstrap/js/bootstrap.bundle.min.js',
+  '/vendor/jquery-easing/jquery.easing.min.js',
+  '/js/jqBootstrapValidation.js',
+  '/js/contact_me.js'
+];
+
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
